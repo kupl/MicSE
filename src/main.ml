ad7c7f59938fa7c40cabc3db01e42dce27a93994
cushionbadak <jsbzwork@gmail.com>
2020-06-01 07:24:19 +0000

    feat(base): add parser example to toy main

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,15 @@
+(*open Tezla*)
+
+let parse : string -> Michelson.Adt.program = fun s ->
+  let lexbuf = Lexing.from_string s in
+  let program : Michelson.Adt.program = Michelson.Parser.start Michelson.Lexer.next_token lexbuf in
+  program
+
 let main : unit -> unit
 =fun () -> begin
-  print_endline ("Hello, World!")
+  print_endline ("Hello, World!");
+  let _ = parse "PUSH nat 0" in
+  print_endline "BYE WORLD";
 end
 
 let _ = main ()
