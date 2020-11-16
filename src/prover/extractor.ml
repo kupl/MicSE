0c843e40402c23c3b6358ff2613d32cd286c38f1
jormal <jormal@naver.com>
2020-11-16 15:26:01 +0000

    fix(extractor): Remove bp from failwith branch Basic path from failwith branch cause some bug because exit variable is not exists. It is removed temporary because it should be fixed in cfg m- odule.

--- src/prover/extractor.ml
+++ src/prover/extractor.ml
@@ -7,8 +7,21 @@ let loop_inv_vtx = ref []
 
 let exit_var = ref None
 
+let read_exit_var : Pre.Lib.Cfg.t -> unit
+=fun cfg -> begin
+  let stmt = Pre.Lib.Cfg.read_stmt_from_vtx cfg cfg.main_exit in
+  match stmt with
+  | Cfg_assign (id, _) -> begin
+      if (Option.is_none !exit_var)
+      then exit_var := Some (id)
+      else if (Option.get !exit_var) <> id then raise (Failure "Extractor.translator: main-exit var conflict")
+    end
+  | _ -> raise (Failure "Extractor.translate: main-exit vertex error")
+end
+
 let rec extract : Pre.Lib.Cfg.t -> Bp.lst
 =fun cfg -> begin
+  let _ = read_exit_var cfg in
   let entry_bp = Bp.create_new_bp cfg.main_entry cfg.main_exit in
   let result = translate entry_bp cfg.main_entry cfg in
   Bp.create_bp_list
@@ -22,20 +35,8 @@ and translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
 =fun cur_bp cur_vtx cfg -> begin
   let stmt = Pre.Lib.Cfg.read_stmt_from_vtx cfg cur_vtx in
   let succ = Pre.Lib.Cfg.read_succ_from_vtx cfg cur_vtx in
-  if Pre.Lib.Cfg.is_main_exit cfg cur_vtx then begin
-    match stmt with
-    | Cfg_assign (id, e) -> begin
-        let assert_inst = create_basic_safety_property cur_vtx e (Pre.Lib.Cfg.CPMap.find_exn cfg.type_info id) in
-        let new_bp = update_current_bp cur_bp assert_inst in
-        let _ = if (Option.is_none !exit_var)
-                then exit_var := Some (id)
-                else if (Option.get !exit_var) <> id then raise (Failure "Extractor.translator: main-exit var conflict") in
-        let inst = Bp.create_inst_assign id e in
-        let new_bp' = update_current_bp new_bp (Some (cur_vtx, inst)) in
-        [new_bp']
-      end
-    | _ -> raise (Failure "Extractor.translate: main-exit vertex error")
-  end else begin
+  if Pre.Lib.Cfg.is_main_exit cfg cur_vtx then [cur_bp]
+  else begin
     match stmt with
     | Cfg_assign (id, e) -> begin
         let assert_inst = create_basic_safety_property cur_vtx e (Pre.Lib.Cfg.CPMap.find_exn cfg.type_info id) in
@@ -92,8 +93,8 @@ and translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
       end
     | Cfg_failwith _ -> begin
         let inst = Bp.create_inst_skip in
-        let new_bp = update_current_bp cur_bp (Some (cur_vtx, inst)) in
-        [new_bp]
+        let _ = update_current_bp cur_bp (Some (cur_vtx, inst)) in
+        [] (* Fail situation must make result with same storage *)
       end
     | Cfg_micse_check_entry -> begin
         let inst = Bp.create_inst_skip in
