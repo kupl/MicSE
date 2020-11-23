1d27414074b909b94c2fc69430d533bb101b6190
cushionbadak <jsbzwork@gmail.com>
2020-11-23 19:51:05 +0900

    feat(analyzer): add available-variable analyzer

---
+++ src/pre/analyzer.mli
@@ -0,0 +1,26 @@
+(*****************************************************************************)
+(*****************************************************************************)
+(* Available Variable Analysis                                               *)
+(*****************************************************************************)
+(*****************************************************************************)
+
+(* For each vertex, get the information of which variables are defined "before" and "after" execute that vertex. *)
+(* Because it does consider whether the variable comes from *)
+
+module AvailVar : sig
+    type vertex = PreLib.Cfg.vertex
+    module CPSet : module type of Core.Set.Poly
+    module CPMap : module type of Core.Map.Poly
+  
+    (* type t = (vertex |-> (available variable "before" execute the stmt) & (available variables "after" execute the stmt)) mapping *)
+    type abs_set = Top | S of string CPSet.t  (* variable set - abstract domain *)
+    val abs_set_join : abs_set -> abs_set -> abs_set
+    val abs_set_meet : abs_set -> abs_set -> abs_set
+    val abs_set_eq : abs_set -> abs_set -> bool
+  
+    type t = (int, abs_set * abs_set) CPMap.t
+    type worklist = vertex CPSet.t
+  
+    val run: PreLib.Cfg.t -> t
+  
+  end (* module AvailVar end *)
---
+++ src/utils/bin/avar_analysis.ml
@@ -0,0 +1,35 @@
+(*****************************************************************************)
+(*****************************************************************************)
+(* Run Available Variable Analyzer                                           *)
+(*****************************************************************************)
+(*****************************************************************************)
+
+(* It preprocesses cfg & run available variable analyzer & show results.
+    It can take same options as MicSE, but the options unrelated with Preprocessing will be ignored.
+    Process returns 0 if normally executed.
+*)
+
+(* Example Usages
+  * run available variable analyzer & print cfg (Run on optimized cfg)
+      dune exec -- micse.utils.avar_analysis -input [PROJECT-ROOT]/benchmarks/toy/add1.tz -cfgopt -cfg_print_dot
+*)
+
+(*  type abs_set = Top | S of string CPSet.t  (* variable set - abstract domain *) *)
+let string_of_absset : Pre.Analyzer.AvailVar.abs_set -> string
+= let open Pre.Analyzer.AvailVar in
+  function
+  | Top -> "AbsSet-Top"
+  | S sset -> CPSet.fold sset ~init:"{" ~f:(fun acc x -> acc ^ ", " ^ x) |> (fun s -> s ^ "}")
+
+let string_of_analyzer_result : (int, Pre.Analyzer.AvailVar.abs_set * Pre.Analyzer.AvailVar.abs_set) Pre.Analyzer.AvailVar.CPMap.t -> string
+= let open Pre.Analyzer.AvailVar in
+  fun r -> CPMap.fold r ~init:"" ~f:(fun ~key ~data acc -> acc ^ "\n" ^ (Stdlib.string_of_int key) ^ "\n  " ^  (string_of_absset (fst data)) ^ "\n  " ^ (string_of_absset (snd data)))
+
+let _ =
+  let _ = Utils.Options.create_options () in
+  let _ = Printexc.record_backtrace false in
+  let (cfg, _) = Pre.pre_process (!Utils.Options.input_file) in
+  let retv = Pre.Analyzer.AvailVar.run cfg in
+  Stdlib.print_endline (string_of_analyzer_result retv);
+  Stdlib.exit 0
+
