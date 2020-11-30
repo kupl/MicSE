63767420a5da60c6c314b4dc16dd6e3caff1c262
jormal <jormal@naver.com>
2020-11-30 14:41:00 +0000

    feat(extractor): Add error handler
    Error handler in extractor is added.
    
    GitHub issue #5

--- src/prover/extractor.ml
+++ src/prover/extractor.ml
@@ -1,5 +1,9 @@
 open ProverLib
 
+exception Error of string
+
+exception InvalidExtraction of (Pre.Lib.Cfg.stmt * string)
+
 (************************************************)
 (************************************************)
 
@@ -14,24 +18,61 @@ let read_exit_var : Pre.Lib.Cfg.t -> unit
   | Cfg_assign (id, _) -> begin
       if (Option.is_none !exit_var)
       then exit_var := Some (id)
-      else if (Option.get !exit_var) <> id then raise (Failure "Extractor.translator: main-exit var conflict")
+      else if (Option.get !exit_var) <> id then raise (Error "translator: main-exit var conflict")
     end
-  | _ -> raise (Failure "Extractor.translate: main-exit vertex error")
+  | _ -> raise (Error "translate: main-exit vertex error")
 end
 
-let rec extract : Pre.Lib.Cfg.t -> Bp.lst
-=fun cfg -> begin
-  let _ = read_exit_var cfg in
-  let entry_bp = Bp.create_new_bp cfg.main_entry cfg.main_exit in
-  let result = translate entry_bp cfg.main_entry cfg in
-  Bp.create_bp_list
-    ~bps:result
-    ~entry:(Bp.create_inv_point ~vtx:cfg.main_entry ~var_opt:(Some (Pre.Lib.Cfg.param_storage_name)))
-    ~exit:(Bp.create_inv_point ~vtx:cfg.main_exit ~var_opt:(Some (Option.get !exit_var)))
-    ~loop:(Core.List.map !loop_inv_vtx ~f:(fun vtx -> Bp.create_inv_point ~vtx:vtx ~var_opt:None))
+(************************************************)
+
+let update_current_bp : Bp.t -> (Bp.vertex * Bp.inst) option -> Bp.t
+=fun cur_bp vtx_inst_opt -> begin
+  match vtx_inst_opt with
+  | None -> cur_bp
+  | Some vtx_inst -> Bp.update_body cur_bp vtx_inst
 end
 
-and translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
+let create_bp_of_branch : Bp.t -> Bp.vertex -> Bp.cond -> (Bp.t * Bp.t)
+=fun cur_bp cur_vtx f_if -> begin
+  let f_else = Bp.create_cond_not f_if in
+  let inst_if = Bp.create_inst_assume f_if in
+  let inst_else = Bp.create_inst_assume f_else in
+  let new_bp_if = update_current_bp cur_bp (Some (cur_vtx, inst_if)) in
+  let new_bp_else = update_current_bp cur_bp (Some (cur_vtx, inst_else)) in
+  (new_bp_if, new_bp_else)
+end
+
+let create_bp_of_loop : Bp.t -> Bp.vertex -> (Bp.t * Bp.t * Bp.t)
+=fun cur_bp loop -> begin
+  let (terminated_bp, new_bp) = Bp.create_cut_bp cur_bp loop in
+  let loop_bp = Bp.create_new_bp loop loop in
+  (terminated_bp, loop_bp, new_bp)
+end
+
+let create_basic_safety_property : Bp.vertex -> Bp.exp -> Bp.typ -> (Bp.vertex * Bp.inst) option
+=fun vtx e t -> begin
+  let loc = Bp.create_loc vtx vtx in
+  match (e, t.d) with
+  | E_add _, T_mutez -> Some (vtx, (Bp.create_inst_assert (Bp.BC_no_overflow e) loc Bp.Q_mutez_arith_safety))
+  | E_sub _, T_mutez -> Some (vtx, (Bp.create_inst_assert (Bp.BC_no_underflow e) loc Bp.Q_mutez_arith_safety))
+  | E_mul _, T_mutez -> Some (vtx, (Bp.create_inst_assert (Bp.BC_no_overflow e) loc Bp.Q_mutez_arith_safety))
+  | _, _ -> None
+end
+
+let read_loc_of_check : Pre.Lib.Cfg.t -> Bp.vertex -> Bp.loc 
+=fun cfg cur_vtx -> begin
+  let pred = Pre.Lib.Cfg.read_pred_from_vtx cfg cur_vtx in
+  let etr_vtxs = Core.List.filter pred ~f:(fun (edge, _) -> Pre.Lib.Cfg.is_edge_check_skip edge) in
+  if Core.List.length etr_vtxs = 1
+  then begin
+    let (_, etr_vtx) = Core.List.hd_exn etr_vtxs in
+    Bp.create_loc etr_vtx cur_vtx
+  end else Bp.create_loc cur_vtx cur_vtx
+end
+
+(************************************************)
+
+let rec translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
 =fun cur_bp cur_vtx cfg -> begin
   let stmt = Pre.Lib.Cfg.read_stmt_from_vtx cfg cur_vtx in
   let succ = Pre.Lib.Cfg.read_succ_from_vtx cfg cur_vtx in
@@ -39,7 +80,11 @@ and translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
   else begin
     match stmt with
     | Cfg_assign (id, e) -> begin
-        let assert_inst = create_basic_safety_property cur_vtx e (Pre.Lib.Cfg.CPMap.find_exn cfg.type_info id) in
+        let typ = (match Core.Map.Poly.find cfg.type_info id with 
+                  | Some typ -> typ
+                  | None -> InvalidExtraction (stmt, ("Type of" ^ id ^ "is invalid")) |> raise
+        ) in
+        let assert_inst = create_basic_safety_property cur_vtx e typ in
         let new_bp = update_current_bp cur_bp assert_inst in
         let inst = Bp.create_inst_assign id e in
         let new_bp' = update_current_bp new_bp (Some (cur_vtx, inst)) in
@@ -111,7 +156,7 @@ and translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
         let result = Core.List.fold_right succ ~f:search ~init:[] in
         result
       end
-    (*| _ -> raise (Failure "Extractor.translate: Not Implemented.")*)
+    (*| _ -> raise (Error "translate: Not Implemented.")*)
   end
 end
 
@@ -119,7 +164,7 @@ and translate_search_normal : Pre.Lib.Cfg.t -> Bp.t -> (Bp.edge * Bp.vertex) -> 
 =fun cfg new_bp (edge, vtx) result -> begin
   match edge with
   | Normal -> result@(translate new_bp vtx cfg)
-  | If_true | If_false -> raise (Failure "Extractor.translate_search_normal: Wrong edge label")
+  | If_true | If_false -> raise (Error "translate_search_normal: Wrong edge label")
   | Check_skip | Failed -> result
 end
 
@@ -128,7 +173,7 @@ and translate_search_branch : Pre.Lib.Cfg.t -> (Bp.t * Bp.t) -> (Bp.edge * Bp.ve
   match edge with
   | If_true -> result@(translate new_bp_if vtx cfg)
   | If_false -> result@(translate new_bp_else vtx cfg)
-  | Normal | Failed -> raise (Failure "Extractor.translate_search_branch: Wrong edge label")
+  | Normal | Failed -> raise (Error "translate_search_branch: Wrong edge label")
   | Check_skip -> result
 end
 
@@ -137,51 +182,22 @@ and translate_search_loop : Pre.Lib.Cfg.t -> (Bp.t * Bp.t) -> (Bp.edge * Bp.vert
   match edge with
   | If_true -> result (* Not implemented *)
   | If_false -> result@(translate new_bp_exit vtx cfg)
-  | Normal | Failed -> raise (Failure "Extractor.translate_search_loop: Wrong edge label")
+  | Normal | Failed -> raise (Error "translate_search_loop: Wrong edge label")
   | Check_skip -> result
 end
 
-and create_bp_of_branch : Bp.t -> Bp.vertex -> Bp.cond -> (Bp.t * Bp.t)
-=fun cur_bp cur_vtx f_if -> begin
-  let f_else = Bp.create_cond_not f_if in
-  let inst_if = Bp.create_inst_assume f_if in
-  let inst_else = Bp.create_inst_assume f_else in
-  let new_bp_if = update_current_bp cur_bp (Some (cur_vtx, inst_if)) in
-  let new_bp_else = update_current_bp cur_bp (Some (cur_vtx, inst_else)) in
-  (new_bp_if, new_bp_else)
-end
-
-and create_bp_of_loop : Bp.t -> Bp.vertex -> (Bp.t * Bp.t * Bp.t)
-=fun cur_bp loop -> begin
-  let (terminated_bp, new_bp) = Bp.create_cut_bp cur_bp loop in
-  let loop_bp = Bp.create_new_bp loop loop in
-  (terminated_bp, loop_bp, new_bp)
-end
-
-and create_basic_safety_property : Bp.vertex -> Bp.exp -> Bp.typ -> (Bp.vertex * Bp.inst) option
-=fun vtx e t -> begin
-  let loc = Bp.create_loc vtx vtx in
-  match (e, t.d) with
-  | E_add _, T_mutez -> Some (vtx, (Bp.create_inst_assert (Bp.BC_no_overflow e) loc Bp.Q_mutez_arith_safety))
-  | E_sub _, T_mutez -> Some (vtx, (Bp.create_inst_assert (Bp.BC_no_underflow e) loc Bp.Q_mutez_arith_safety))
-  | E_mul _, T_mutez -> Some (vtx, (Bp.create_inst_assert (Bp.BC_no_overflow e) loc Bp.Q_mutez_arith_safety))
-  | _, _ -> None
-end
-
-and read_loc_of_check : Pre.Lib.Cfg.t -> Bp.vertex -> Bp.loc 
-=fun cfg cur_vtx -> begin
-  let pred = Pre.Lib.Cfg.read_pred_from_vtx cfg cur_vtx in
-  let etr_vtxs = Core.List.filter pred ~f:(fun (edge, _) -> Pre.Lib.Cfg.is_edge_check_skip edge) in
-  if Core.List.length etr_vtxs = 1
-  then begin
-    let (_, etr_vtx) = Core.List.hd_exn etr_vtxs in
-    Bp.create_loc etr_vtx cur_vtx
-  end else Bp.create_loc cur_vtx cur_vtx
-end
-
-and update_current_bp : Bp.t -> (Bp.vertex * Bp.inst) option -> Bp.t
-=fun cur_bp vtx_inst_opt -> begin
-  match vtx_inst_opt with
-  | None -> cur_bp
-  | Some vtx_inst -> Bp.update_body cur_bp vtx_inst
+let extract : Pre.Lib.Cfg.t -> Bp.lst
+=fun cfg -> begin
+  try
+    let _ = read_exit_var cfg in
+    let entry_bp = Bp.create_new_bp cfg.main_entry cfg.main_exit in
+    let result = translate entry_bp cfg.main_entry cfg in
+    Bp.create_bp_list
+      ~bps:result
+      ~entry:(Bp.create_inv_point ~vtx:cfg.main_entry ~var_opt:(Some (Pre.Lib.Cfg.param_storage_name)))
+      ~exit:(Bp.create_inv_point ~vtx:cfg.main_exit ~var_opt:(Some (Option.get !exit_var)))
+      ~loop:(Core.List.map !loop_inv_vtx ~f:(fun vtx -> Bp.create_inv_point ~vtx:vtx ~var_opt:None))
+  with
+  | InvalidExtraction (stmt, msg) -> Error ("Invalid extraction of statement [" ^ (stmt |> Pre.Lib.Cfg.stmt_to_str) ^ "]: " ^ msg) |> raise
+  | e -> e |> raise
 end
--- src/prover/extractor.mli
+++ src/prover/extractor.mli
@@ -1,26 +1,29 @@
-open ProverLib
+exception Error of string
+
+exception InvalidExtraction of (Pre.Lib.Cfg.stmt * string)
 
 (************************************************)
 (************************************************)
 
-val loop_inv_vtx : (Bp.vertex list) ref
+val loop_inv_vtx : (ProverLib.Bp.vertex list) ref
 
-val exit_var : (Bp.var option) ref
+val exit_var : (ProverLib.Bp.var option) ref
+val read_exit_var : Pre.Lib.Cfg.t -> unit
 
-val extract : Pre.Lib.Cfg.t -> Bp.lst
+(************************************************)
 
-val translate : Bp.t -> Bp.vertex -> Pre.Lib.Cfg.t -> Bp.t list
+val update_current_bp : ProverLib.Bp.t -> (ProverLib.Bp.vertex * ProverLib.Bp.inst) option -> ProverLib.Bp.t
 
-val translate_search_normal : Pre.Lib.Cfg.t -> Bp.t -> (Bp.edge * Bp.vertex) -> Bp.t list -> Bp.t list
+val create_bp_of_branch : ProverLib.Bp.t -> ProverLib.Bp.vertex -> ProverLib.Bp.cond -> (ProverLib.Bp.t * ProverLib.Bp.t)
+val create_bp_of_loop : ProverLib.Bp.t -> ProverLib.Bp.vertex -> (ProverLib.Bp.t * ProverLib.Bp.t * ProverLib.Bp.t)
+val create_basic_safety_property : ProverLib.Bp.vertex -> ProverLib.Bp.exp -> ProverLib.Bp.typ -> (ProverLib.Bp.vertex * ProverLib.Bp.inst) option
 
-val translate_search_branch : Pre.Lib.Cfg.t -> (Bp.t * Bp.t) -> (Bp.edge * Bp.vertex) -> Bp.t list -> Bp.t list
+val read_loc_of_check : Pre.Lib.Cfg.t -> ProverLib.Bp.vertex -> ProverLib.Bp.loc 
 
-val create_bp_of_branch : Bp.t -> Bp.vertex -> Bp.cond -> (Bp.t * Bp.t)
+(************************************************)
 
-val create_bp_of_loop : Bp.t -> Bp.vertex -> (Bp.t * Bp.t * Bp.t)
+val translate : ProverLib.Bp.t -> ProverLib.Bp.vertex -> Pre.Lib.Cfg.t -> ProverLib.Bp.t list
+val translate_search_normal : Pre.Lib.Cfg.t -> ProverLib.Bp.t -> (ProverLib.Bp.edge * ProverLib.Bp.vertex) -> ProverLib.Bp.t list -> ProverLib.Bp.t list
+val translate_search_branch : Pre.Lib.Cfg.t -> (ProverLib.Bp.t * ProverLib.Bp.t) -> (ProverLib.Bp.edge * ProverLib.Bp.vertex) -> ProverLib.Bp.t list -> ProverLib.Bp.t list
 
-val create_basic_safety_property : Bp.vertex -> Bp.exp -> Bp.typ -> (Bp.vertex * Bp.inst) option
-
-val read_loc_of_check : Pre.Lib.Cfg.t -> Bp.vertex -> Bp.loc 
-
-val update_current_bp : Bp.t -> (Bp.vertex * Bp.inst) option -> Bp.t
+val extract : Pre.Lib.Cfg.t -> ProverLib.Bp.lst
