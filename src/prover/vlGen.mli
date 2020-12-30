69e69821d57e75925bb0fecf6ce5fe0f0c5733ee
cushionbadak <jsbzwork@gmail.com>
2020-12-30 16:14:41 +0900

    feat(vlGen): add cfg.expr -> vlang.expr converter

--- src/prover/vlGen.mli
+++ src/prover/vlGen.mli
@@ -1,4 +1,6 @@
 (* Vlang generator (from Cfg Stmt & Cfg Expression). 
     This module does not consider any variable renaming issues *)
 
-val expr_of_cfgexpr : ProverLib.GlVar.Env.t -> PreLib.Cfg.expr -> ProverLib.Vlang.Expr.t
+exception InvalidConversion_Expr of PreLib.Cfg.expr
+
+val expr_of_cfgexpr : ProverLib.GlVar.Env.t -> PreLib.Cfg.t -> PreLib.Cfg.expr -> ProverLib.Vlang.Expr.t
