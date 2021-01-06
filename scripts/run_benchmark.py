efbe32d0c2601a6322509fe8522cd4aab7bc5541
jormal <jormal@naver.com>
2021-01-06 10:49:39 +0000

    feat(script): Add time budget option
    Time budget option is added to benchmark running script.
    
    GitHub issue #18

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -34,6 +34,8 @@ def read_env ():
   parser = argparse.ArgumentParser ()
   parser.add_argument ("-b", "--benchmark", type=str, default="toy", help="benchmark directory which to examinate (default: toy)")
   parser.add_argument ("-o", "--output", type=str, default="", help="output file name (default: same with benchmark option)")
+  parser.add_argument ("-z", "--z3-time-budget", type=int, default=30, help="time budget for Z3 solver in second (default: 30s)")
+  parser.add_argument ("-p", "--prover-time-budget", type=int, default=180, help="time budget for whole prover process in second (default: 180s)")
   args = parser.parse_args ()
   # Initialize env
   ## Add env for benchmark directory path and candidates
@@ -50,6 +52,9 @@ def read_env ():
   else:
     env.output = os.path.join (OUTPUT_DIR, args.output)
   env.output += str(datetime.now().strftime("_%Y%m%d%H%M%S.out"))
+  ## Add env for time budget
+  env.z3_time = args.z3_time_budget
+  env.prover_time = args.prover_time_budget
   return env
 
 def run (env):
@@ -65,6 +70,8 @@ def run (env):
       # Set command line
       cmd = ["dune", "exec", "--", "micse"]
       cmd += ["-input", os.path.join (env.benchmark, file)]
+      cmd += ["-z3_timeout", str (env.z3_time)]
+      cmd += ["-prover_timeout", str (env.prover_time)]
       # Job Started
       print ("  > Job [{}/{}] is started".format (idx + 1, jobLen))
       fp.write ("$ " + " ".join (cmd) + "\n")
@@ -75,7 +82,7 @@ def run (env):
       # Job Finished
       runTime = round ((timeFinished - timeStarted), 3)
       fp.write (proc.stdout)
-      fp.write ("...[{}s]\n\n".format (runTime))
+      fp.write ("...[{}s - {}/{}]\n\n".format (runTime, idx + 1, jobLen))
       print ("  > Job [{}/{}] is done in {}s".format (idx + 1, jobLen, runTime))
   return None
 
