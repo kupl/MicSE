b766fd673a11bd4082df08d41b903f9fc89b5d2e
jormal <jormal@naver.com>
2021-01-14 03:35:50 +0000

    fix(invRecipe): Edit name of remain variable
    Remain variable name of partial sum sub formula is edited.

--- src/prover/lib/invRecipe.ml
+++ src/prover/lib/invRecipe.ml
@@ -57,8 +57,8 @@ let mutez_equal : Vlang.Component.t -> Vlang.Formula.t CPSet.t
 end (* function formula_mutez_equal end *)
 
 (* WARNING : "remain_var_gen" has internal side-effects. *)
-let remain_var_gen : Vlang.Expr.t -> string
-= fun e -> ("__MTZMAP_SUM_R_(" ^ (e |> Vlang.Expr.to_string) ^ ")")
+let remain_var_gen : Vlang.Expr.t -> Vlang.Expr.t list -> string
+= fun e el -> ("__MTZMAP_SUM_R_(" ^ (e |> Vlang.Expr.to_string) ^ ")_(" ^ (el |> Core.List.map ~f:Vlang.Expr.to_string |> Core.String.concat ~sep:")_(") ^ ")")
 
 let mtzmap_partial_sum : Vlang.Component.t -> Vlang.Component.t -> Vlang.Component.t -> Vlang.Formula.t CPSet.t
 = let open Vlang in
@@ -81,6 +81,7 @@ let mtzmap_partial_sum : Vlang.Component.t -> Vlang.Component.t -> Vlang.Compone
         ] 
       in
       (* generate formula *)
-      VF_imply (fold_precond prec_lst, VF_mtzmap_partial_sum_equal(km.body, CPSet.fold k_sset ~init:[] ~f:(fun accl x -> x.body :: accl), m.body, remain_var_gen km.body))
+      let k_slist = CPSet.fold k_sset ~init:[] ~f:(fun accl x -> x.body :: accl) in
+      VF_imply (fold_precond prec_lst, VF_mtzmap_partial_sum_equal(km.body, k_slist, m.body, remain_var_gen km.body k_slist))
     )
 end (* function mtzmap_partial_sum end *)
