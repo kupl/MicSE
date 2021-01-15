85e9b27329c6f742e89825b9b18eddd0f7d68d9c
jormal <jormal@naver.com>
2021-01-15 10:56:03 +0000

    fix(verifier): Fix a bug from Z3 mul_no_overflow
    `Z3.BitVector.mk_mul_no_overflow` has a bug and it is fixed.
    
    GitHub issue #23

--- src/prover/verifier.ml
+++ src/prover/verifier.ml
@@ -438,10 +438,39 @@ let rec smtexpr_of_vlangformula : Vlang.t -> Smt.ZFormula.t
       | VF_mich_iter_m _ -> err vf (* check whether map's size is not 0 *)
       | VF_mich_micse_check_value e -> Smt.ZBool.create_eq (e |> soe) (Smt.ZBool.true_ ())
       (* Custom Formula for verifiying *)
-      | VF_add_mmm_no_overflow (e1, e2) -> Smt.ZMutez.check_add_no_overflow (e1 |> soe) (e2 |> soe)
-      | VF_sub_mmm_no_underflow (e1, e2) -> Smt.ZMutez.check_sub_no_underflow (e1 |> soe) (e2 |> soe)
-      | VF_mul_mnm_no_overflow (e1, e2) -> Smt.ZMutez.check_mul_no_overflow (e1 |> soe) (e2 |> soe |> Smt.ZInt.to_zmutez)
-      | VF_mul_nmm_no_overflow (e1, e2) -> Smt.ZMutez.check_mul_no_overflow (e1 |> soe |> Smt.ZInt.to_zmutez) (e2 |> soe)
+      | VF_add_mmm_no_overflow (e1, e2) -> begin
+          let soe1, soe2 = (e1 |> soe), (e2 |> soe) in
+          Smt.ZMutez.create_ge (Smt.ZMutez.create_add soe1 soe2) soe1
+          (* Smt.ZMutez.check_add_no_overflow (e1 |> soe) (e2 |> soe) *)
+        end
+      | VF_sub_mmm_no_underflow (e1, e2) -> begin
+          Smt.ZMutez.create_ge (e1 |> soe) (e2 |> soe)
+          (* Smt.ZMutez.check_sub_no_underflow (e1 |> soe) (e2 |> soe) *)
+        end
+      | VF_mul_mnm_no_overflow (e1, e2) -> begin
+          let soe1, soe2 = (e1 |> soe), (e2 |> soe |> Smt.ZInt.to_zmutez) in
+          let e1_mul_e2 = Smt.ZMutez.create_mul soe1 soe2 in  (* e1 * e2 *)
+          let e1_mul_e2_div_e1 = Smt.ZMutez.create_div e1_mul_e2 soe2 in (* (e1 * e2) / e1 *)
+          let e1_is_zero = Smt.ZMutez.create_eq soe1 (Smt.ZMutez.zero_ ()) in (* e1 = 0 *)
+          let e1_is_not_zero = Smt.ZFormula.create_and [ (* e1 != 0 /\ ((e1 * e2) / e1) = e2 *)
+            (Smt.ZFormula.create_not e1_is_zero);
+            (Smt.ZMutez.create_eq e1_mul_e2_div_e1 soe1)
+          ] in
+          Smt.ZFormula.create_or [e1_is_zero; e1_is_not_zero] (* (e1 = 0) \/ (e1 != 0 /\ ((e1 * e2) / e1) = e2) *)
+          (* Smt.ZMutez.check_mul_no_overflow (e1 |> soe) (e2 |> soe |> Smt.ZInt.to_zmutez) *)
+        end
+      | VF_mul_nmm_no_overflow (e1, e2) -> begin
+          let soe1, soe2 = (e1 |> soe |> Smt.ZInt.to_zmutez), (e2 |> soe) in
+          let e1_mul_e2 = Smt.ZMutez.create_mul soe1 soe2 in  (* e1 * e2 *)
+          let e1_mul_e2_div_e1 = Smt.ZMutez.create_div e1_mul_e2 soe2 in (* (e1 * e2) / e1 *)
+          let e1_is_zero = Smt.ZMutez.create_eq soe1 (Smt.ZMutez.zero_ ()) in (* e1 = 0 *)
+          let e1_is_not_zero = Smt.ZFormula.create_and [ (* e1 != 0 /\ ((e1 * e2) / e1) = e2 *)
+            (Smt.ZFormula.create_not e1_is_zero);
+            (Smt.ZMutez.create_eq e1_mul_e2_div_e1 soe1)
+          ] in
+          Smt.ZFormula.create_or [e1_is_zero; e1_is_not_zero] (* (e1 = 0) \/ (e1 != 0 /\ ((e1 * e2) / e1) = e2) *)
+          (* Smt.ZMutez.check_mul_no_overflow (e1 |> soe |> Smt.ZInt.to_zmutez) (e2 |> soe) *)
+        end
       | VF_shiftL_nnn_rhs_in_256 _ -> err vf
       | VF_shiftR_nnn_rhs_in_256 _ -> err vf
       (* Custom Domain Formula for Invariant Generation *)
