f34685085e9cf1caa040621bf4650fc16d742378
Heewoong Jang <jormal@naver.com>
2021-04-05 18:05:12 +0900

    feat(invSyn): Add component of mich_v
    The functions for component of mich_v are added.

--- src/stacked/invSyn.ml
+++ src/stacked/invSyn.ml
@@ -38,6 +38,99 @@ type ingredients = {
 
 (*****************************************************************************)
 (*****************************************************************************)
+(* Component Collector                                                       *)
+(*****************************************************************************)
+(*****************************************************************************)
+
+type component = {
+  precond_lst : Tz.mich_f list;
+  typ         : Tz.mich_t Tz.cc;
+  body        : Tz.mich_v Tz.cc; 
+}
+
+let fold_precond : Tz.mich_f list -> Tz.mich_f
+= fun prec_lst -> begin
+  (* fold_precond function start *)
+  Tz.MF_and prec_lst
+  (* fold_precond function end *)
+end 
+
+let comp_of_val : Tz.mich_f list * Tz.mich_v Tz.cc -> component
+= fun (pcl, v) -> begin
+  (* comp_of_val function start *)
+  { precond_lst=pcl; typ=(Tz.typ_of_val v); body=v; }
+  (* comp_of_val function end *)
+end
+
+let collect_components : Tz.mich_f list * Tz.mich_v Tz.cc -> component set
+= (* inner function of component collecting *)
+  let rec collect_components_i : component -> component set -> component set
+  = let open Tz in
+    fun cur_comp acc -> begin
+    (* collect_components_i function start *)
+    let { precond_lst=cur_prec_lst; typ=cur_typ; body=cur_body; } = cur_comp in
+    match cur_typ.cc_v with
+    | MT_option t1cc -> begin
+      let precond_none = (MF_is_none cur_body) :: cur_prec_lst in
+      let precond_some = (MF_not (MF_is_none cur_body)) :: cur_prec_lst in
+      let body_unlifted = gen_dummy_cc (MV_unlift_option cur_body) in
+      let comp_none = { precond_lst=precond_none; typ=cur_typ; body=(gen_dummy_cc (MV_none t1cc)); } in
+      let comp_some = { precond_lst=precond_some; typ=cur_typ; body=(gen_dummy_cc (MV_some body_unlifted)); } in
+      let comp_some_unlifted = { precond_lst=precond_some; typ=cur_typ; body=body_unlifted; } in
+      collect_components_i comp_some_unlifted (PSet.add (PSet.add acc comp_none) comp_some)
+      end
+    | MT_pair (t1cc, t2cc) -> begin
+      let comp_pair = cur_comp in
+      let comp_fst = { precond_lst=cur_prec_lst; typ=t1cc; body=(gen_dummy_cc (MV_car cur_body)); } in
+      let comp_snd = { precond_lst=cur_prec_lst; typ=t2cc; body=(gen_dummy_cc (MV_cdr cur_body)); } in
+      let acc_fst = collect_components_i comp_fst (PSet.add acc comp_pair) in
+      collect_components_i comp_snd acc_fst
+      end
+    | MT_or (t1cc, t2cc) -> begin
+      let precond_left = (MF_is_left cur_body) :: cur_prec_lst in
+      let precond_right = (MF_not (MF_is_left cur_body)) :: cur_prec_lst in
+      let body_left_unlifted = gen_dummy_cc (MV_unlift_left cur_body) in
+      let body_right_unlifted = gen_dummy_cc (MV_unlift_right cur_body) in
+      let comp_left = { precond_lst=precond_left; typ=cur_typ; body=(gen_dummy_cc (MV_left (cur_typ, body_left_unlifted))); } in
+      let comp_right = { precond_lst=precond_right; typ=cur_typ; body=(gen_dummy_cc (MV_right (cur_typ, body_right_unlifted))); } in
+      let comp_left_unlifted = { precond_lst=precond_left; typ=t1cc; body=body_left_unlifted; } in
+      let comp_right_unlifted = { precond_lst=precond_right; typ=t2cc; body=body_right_unlifted; } in
+      let acc_left = collect_components_i comp_left_unlifted (PSet.add (PSet.add acc comp_left) comp_right) in
+      collect_components_i comp_right_unlifted acc_left
+      end
+    | _ -> PSet.add acc cur_comp
+    (* collect_components_i function end *)
+  end in
+  fun v_info -> begin
+  (* collect_components function start *)
+  collect_components_i (comp_of_val v_info) PSet.empty
+  (* collect_components function end *)
+end
+
+let filter_comp : (Tz.mich_t -> bool) -> component set -> component set
+= fun filter_f cset -> begin
+  (* filter_comp function start *)
+  PSet.filter cset ~f:(fun c -> filter_f c.typ.cc_v)
+  (* filter_comp function end *)
+end
+
+let classify_comp_with_type : component set -> (Tz.mich_t, component set) map
+= fun cset -> begin
+  (* classify_comp_with_type function start *)
+  PSet.fold
+    cset
+    ~init:PMap.empty
+    ~f:(fun acc c -> (
+          PMap.update
+            acc
+            c.typ.cc_v
+            ~f:(function None -> PSet.singleton c | Some cset -> PSet.add cset c)))
+  (* classify_comp_with_type function end *)
+end
+
+
+(*****************************************************************************)
+(*****************************************************************************)
 (* Synthesizer                                                               *)
 (*****************************************************************************)
 (*****************************************************************************)
