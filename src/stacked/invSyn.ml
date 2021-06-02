a3517f6e8d98491db6401207169b6b65d92bc396
Heewoong Jang <jormal@naver.com>
2021-06-03 02:24:36 +0900

    chore: Reduce invariant recipe
    Some invariant recipe which is not necessary is reduced.

--- src/stacked/invSyn.ml
+++ src/stacked/invSyn.ml
@@ -336,13 +336,13 @@ let refine_t : Se.invmap -> ingredients -> Tz.mich_f Core.Set.Poly.t
   let ctmap = igdt.igdt_comp_type_map in
   (* 1. generate recipe *)
   let all_eq_fmlas : Tz.mich_f CPSet.t = all_equal ctmap in
-  let all_ge_fmlas : Tz.mich_f CPSet.t = all_ge ctmap [MT_int; MT_nat; MT_mutez] in
-  let all_gt_fmlas : Tz.mich_f CPSet.t = all_gt ctmap [MT_int; MT_nat; MT_mutez] in
+  (* let all_ge_fmlas : Tz.mich_f CPSet.t = all_ge ctmap [MT_int; MT_nat; MT_mutez] in *)
+  (* let all_gt_fmlas : Tz.mich_f CPSet.t = all_gt ctmap [MT_int; MT_nat; MT_mutez] in *)
   let add_2_eq_fmlas : Tz.mich_f CPSet.t = add_2_eq ctmap [MT_mutez] in
   (* 2. generate invariant map *)
   [ all_eq_fmlas;
-    all_ge_fmlas;
-    all_gt_fmlas;
+    (* all_ge_fmlas; *)
+    (* all_gt_fmlas; *)
     add_2_eq_fmlas; ]
   |> collect_set
   |> CPSet.filter
@@ -363,13 +363,13 @@ let refine_l : Se.invmap -> ingredients -> Tz.mich_f Core.Set.Poly.t
   let ctmap = igdt.igdt_comp_type_map in
   (* 1. generate recipe *)
   let all_eq_fmlas : Tz.mich_f CPSet.t = all_equal ctmap in
-  let all_ge_fmlas : Tz.mich_f CPSet.t = all_ge ctmap [MT_int; MT_nat; MT_mutez] in
-  let all_gt_fmlas : Tz.mich_f CPSet.t = all_gt ctmap [MT_int; MT_nat; MT_mutez] in
+  (* let all_ge_fmlas : Tz.mich_f CPSet.t = all_ge ctmap [MT_int; MT_nat; MT_mutez] in *)
+  (* let all_gt_fmlas : Tz.mich_f CPSet.t = all_gt ctmap [MT_int; MT_nat; MT_mutez] in *)
   let add_2_eq_fmlas : Tz.mich_f CPSet.t = add_2_eq ctmap [MT_mutez] in
   (* 2. generate invariant map *)
   [ all_eq_fmlas;
-    all_ge_fmlas;
-    all_gt_fmlas;
+    (* all_ge_fmlas; *)
+    (* all_gt_fmlas; *)
     add_2_eq_fmlas; ]
   |> collect_set
   |> CPSet.filter
