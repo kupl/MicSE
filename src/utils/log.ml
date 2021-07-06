6fab3f0f8ffddd1e0a9c86e9cbb11c35dacd91d9
Heewoong Jang <jormal@naver.com>
2021-07-06 18:10:09 +0900

    fix(Log): Add flush on logging
    Flushing is added on logging.

--- src/utils/log.ml
+++ src/utils/log.ml
@@ -12,32 +12,24 @@ module Setting = struct
   
   let reporter : unit -> Logs.reporter
   = fun () -> begin
-    (* reporter function start *)
     let report = fun _ level ~over k msgf -> begin
-      (* report function start *)
       let k' = fun _ -> begin
-        (* k' function start *)
         over (); k ()
-        (* k' function end *)
-      end in
+      end in (* k' function end *)
       let dt = log_counter |> Mtime_clock.count |> Mtime.Span.to_ms in
       msgf @@ (fun ?header ?tags fmt -> (
         ignore (tags);
         match level with
         | Logs.App  -> Format.kfprintf k' Format.std_formatter ("%a@[" ^^ fmt ^^ "@]@.") Logs.pp_header (level, header)
         | _         -> Format.kfprintf k' Format.err_formatter ("%a[%0+7.0fms] @[" ^^ fmt ^^ "@]@.") Logs.pp_header (level, header) dt))
-      (* report function end *)
-    end in
+    end in (* report function end *)
     { Logs.report = report }
-    (* reporter function end *)
-  end
+  end (* reporter function end *)
 
   let msg : Logs.level -> 'a Logs.log
   = fun level log -> begin
-    (* msg function start *)
     Logs.msg level log
-    (* msg function end *)
-  end
+  end (* msg function end *)
 end
 
 
@@ -49,48 +41,41 @@ end
 
 let app : ('a, unit) Logs.msgf -> unit
 = fun log -> begin
-  (* app function start *)
-  Setting.msg Logs.App log
-  (* app function end *)
-end
+  Setting.msg Logs.App log;
+  Stdlib.flush Stdlib.stdout
+end (* function app end *)
 
 let debug : ('a, unit) Logs.msgf -> unit
 = fun log -> begin
-  (* info function start *)
-  Setting.msg Logs.Debug log
-  (* info function end *)
-end
+  Setting.msg Logs.Debug log;
+  Stdlib.flush Stdlib.stderr
+end (* function info end *)
 
 
 let info : ('a, unit) Logs.msgf -> unit
 = fun log -> begin
-  (* info function start *)
-  Setting.msg Logs.Info log
-  (* info function end *)
-end
+  Setting.msg Logs.Info log;
+  Stdlib.flush Stdlib.stderr
+end (* function info end *)
 
 let warn : ('a, unit) Logs.msgf -> unit
 = fun log -> begin
-  (* warn function start *)
-  Setting.msg Logs.Warning log
-  (* warn function end *)
-end
+  Setting.msg Logs.Warning log;
+  Stdlib.flush Stdlib.stderr
+end (* function warn end *)
 
 let err : ('a, unit) Logs.msgf -> unit
 = fun log -> begin
-  (* err function start *)
-  Setting.msg Logs.Error log
-  (* err function end *)
-end
+  Setting.msg Logs.Error log;
+  Stdlib.flush Stdlib.stderr
+end (* function err end *)
 
 let create : unit -> unit
 = fun () -> begin
-  (* create function start *)
   Logs.set_reporter (Setting.reporter ());
   if !Options.flag_debug then (Logs.set_level (Some Logs.Debug))
   else if !Options.flag_verbose then (Logs.set_level (Some Logs.Info))
   else (Logs.set_level (Some Logs.Warning));
   info (fun m -> m "Logger Start.");
   ()
-  (* create function end *)
-end
+end (* function create end *)
