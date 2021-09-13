1618cc669162e04540f112edc687854f7a9c574b
cushionbadak <jsbzwork@gmail.com>
2021-09-13 18:49:03 +0900

    feat(SexpUtil): Add remove-ctx-i functionality.

--- lib/sexpUtil.ml
+++ lib/sexpUtil.ml
@@ -12,3 +12,11 @@ let rec tz_cc_sexp_form : Sexp.t -> Sexp.t =
      tz_cc_sexp_form body
    | Atom _ as s -> s
    | List sl -> List (List.map ~f:tz_cc_sexp_form sl)
+
+let rec tz_remove_ctx_i_ctx_v : Sexp.t -> Sexp.t =
+   let open Sexp in
+   function
+   | List [ List [ Atom "ctx_i"; _ ]; List [ Atom "ctx_v"; body ] ] ->
+     tz_remove_ctx_i_ctx_v body
+   | Atom _ as s -> s
+   | List sl -> List (List.map ~f:tz_remove_ctx_i_ctx_v sl)
