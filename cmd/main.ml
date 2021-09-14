bb13c83bfeec72b6c78cfd00ba2d4daaf6931613
cushionbadak <jsbzwork@gmail.com>
2021-09-14 15:38:53 +0900

    feat(ExecFlow): Add turn-base prover/refuter

--- cmd/main.ml
+++ cmd/main.ml
@@ -1,26 +1,11 @@
 exception Error of string
 
-let main : unit -> unit =
-  fun () ->
-  let (mich_pgm, mich_init_strg_opt) = Lib.ExecFlow.parsing () in
-  let (tz_pgm, _) = Lib.ExecFlow.tz_rep (mich_pgm, mich_init_strg_opt) in
-  let _ = Lib.ExecFlow.sym_exec tz_pgm in
-  ()
-
 let _ =
-   (* 1. Initialize Input Arguments *)
-   Utils.Argument.create None;
-   (* 2. Initialize Logger *)
-   Utils.Log.create ();
-   (* 3. Enable Error Backtrace *)
-   Printexc.record_backtrace true;
-   try
-     (* 4. Check Input File Path Nullity *)
-     if !Utils.Argument.input_file <> ""
-     then main ()
-     else Stdlib.raise (Error "No input.")
-   with
-   | exc ->
-     Utils.Log.err (fun m ->
-         m "%s\n%s" (exc |> Printexc.to_string) (Printexc.get_backtrace ())
-     )
+   let _ = Utils.Log.app (fun m -> m "MicSE Start") in
+   let (cfg, res) = Lib.ExecFlow.prover_refuter_toss None in
+   let _ =
+      Utils.Log.app (fun m ->
+          m "Final-Report : %s" (Lib.Res.string_of_res_rough cfg res)
+      )
+   in
+   ()
--- lib/execFlow.ml
+++ lib/execFlow.ml
@@ -119,7 +119,6 @@ let refuter_naive_run : string array option -> Res.config * Res.res =
   let res = Refute.naive_run cfg init_res in
   (cfg, res)
 
-
 let prover_naive_run : string array option -> Res.config * Res.res =
   fun argv_opt ->
   let sym_exec_res = upto_sym_exec argv_opt in
@@ -146,3 +145,79 @@ let prover_naive_run : string array option -> Res.config * Res.res =
   let init_res = Res.init_res cfg in
   let res = Prove.naive_run cfg init_res in
   (cfg, res)
+
+let prover_refuter_toss : string array option -> Res.config * Res.res =
+   let open Res in
+   let escape_cond : Res.config -> Res.res -> bool =
+     fun { cfg_timer; cfg_memory; _ } { r_qr_lst; _ } ->
+     Utils.Time.is_timeout cfg_timer
+     || Utils.Memory.is_memoryout cfg_memory
+     || List.for_all
+          (fun { qr_prv_flag; qr_rft_flag; _ } ->
+            equal_prover_flag qr_prv_flag PF_p
+            || equal_refuter_flag qr_rft_flag RF_r
+            || equal_prover_flag qr_prv_flag PF_f
+               && equal_refuter_flag qr_rft_flag RF_f)
+          r_qr_lst
+   in
+   let rec prover_refuter_toss_i : Res.config -> Res.res -> Res.res =
+     fun cfg r ->
+     if escape_cond cfg r
+     then r
+     else
+       r
+       |> (fun rv ->
+            let _ =
+               Utils.Log.app (fun m ->
+                   m "Mid-Report: %s" (Res.string_of_res_rough cfg rv)
+               )
+            in
+            rv)
+       |> (fun rv ->
+            let _ = Utils.Log.app (fun m -> m "Prover Turn Start") in
+            rv)
+       |> Prove.naive_run_res_atomic_action cfg
+       |> (fun rv ->
+            let _ = Utils.Log.app (fun m -> m "Prover Turn End") in
+            rv)
+       |> (fun rv ->
+            let _ =
+               Utils.Log.app (fun m ->
+                   m "Mid-Report: %s" (Res.string_of_res_rough cfg rv)
+               )
+            in
+            rv)
+       |> (fun rv ->
+            let _ = Utils.Log.app (fun m -> m "Refuter Turn Start") in
+            rv)
+       |> Refute.naive_run_res_atomic_action cfg
+       |> (fun rv ->
+            let _ = Utils.Log.app (fun m -> m "Refuter Turn End") in
+            rv)
+       |> prover_refuter_toss_i cfg
+   in
+   fun argv_opt ->
+   let sym_exec_res = upto_sym_exec argv_opt in
+   let (_, init_strg_opt, se_result, init_state) = sym_exec_res in
+   let cfg = Res.init_config init_strg_opt se_result init_state in
+   let _ =
+      (* cfg.cfg_m_view debugging info *)
+      let mv = cfg.cfg_m_view in
+      let open Se in
+      let module RMCIMap = Se.SSGraph.RMCIMap in
+      let _ =
+         RMCIMap.iteri mv ~f:(fun ~key ~data:x ->
+             let l = SSet.length in
+             let (p, s) = (x.pred, x.succ) in
+             Utils.Log.debug (fun m ->
+                 m "%s : %d %d"
+                   (Tz.sexp_of_r_mich_cut_info key |> Core.Sexp.to_string)
+                   (l p) (l s)
+             )
+         )
+      in
+      ()
+   in
+   let init_res = Res.init_res cfg in
+   let res = prover_refuter_toss_i cfg init_res in
+   (cfg, res)
