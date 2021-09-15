1b85eadc3b05e7d052822c5951f0cc345909557c
Heewoong Jang <jormal@naver.com>
2021-09-15 06:03:34 +0000

    chore: Add error catch flow
    Error catch flow is added.

--- cmd/main.ml
+++ cmd/main.ml
@@ -1,11 +1,17 @@
 exception Error of string
 
 let _ =
-   let _ = Utils.Log.app (fun m -> m "MicSE Start") in
-   let (cfg, res) = Lib.ExecFlow.prover_refuter_toss None in
-   let _ =
-      Utils.Log.app (fun m ->
-          m "Final-Report : %s" (Lib.Res.string_of_res cfg res)
-      )
-   in
-   ()
+   try
+     let _ = Utils.Log.app (fun m -> m "MicSE Start") in
+     let (cfg, res) = Lib.ExecFlow.prover_refuter_toss None in
+     let _ =
+        Utils.Log.app (fun m ->
+            m "Final-Report : %s" (Lib.Res.string_of_res cfg res)
+        )
+     in
+     ()
+   with
+   | exc ->
+     Utils.Log.err (fun m ->
+         m "%s\n%s" (exc |> Printexc.to_string) (Printexc.get_backtrace ())
+     )
--- cmd/naive_refuter.ml
+++ cmd/naive_refuter.ml
@@ -1,6 +1,12 @@
 exception Error of string
 
-let _ = 
-  let (cfg, res) = Lib.ExecFlow.refuter_naive_run None in
-  let _ = Utils.Log.app (fun m -> m "%s" (Lib.Res.string_of_res cfg res)) in
-  ()
+let _ =
+   try
+     let (cfg, res) = Lib.ExecFlow.refuter_naive_run None in
+     let _ = Utils.Log.app (fun m -> m "%s" (Lib.Res.string_of_res cfg res)) in
+     ()
+   with
+   | exc ->
+     Utils.Log.err (fun m ->
+         m "%s\n%s" (exc |> Printexc.to_string) (Printexc.get_backtrace ())
+     )
